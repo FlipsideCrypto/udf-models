99cb65f0c375770ce0caae475176c996c8ac894b
austin <austin@flipsidecrypto.com>
2023-01-17 14:05:23 -0500

    decoder v1 - dev

--- macros/sp_add_abi_for_decoding.sql
+++ macros/sp_add_abi_for_decoding.sql
@@ -1,14 +1,14 @@
-{% macro sp_add_abi_for_decoding(target_schema) -%}
+{% macro sp_add_abi_for_decoding() -%}
     CREATE
-    OR REPLACE PROCEDURE {{ target_schema }}.sp_add_contract_for_decoding(
+    OR REPLACE PROCEDURE streamline.sp_add_contract_for_decoding(
         contract_address VARCHAR,
         blockchain VARCHAR,
         discord_username VARCHAR,
         abi VARCHAR
     ) returns VARCHAR NOT NULL LANGUAGE SQL AS $$
 BEGIN
-    CREATE schema if NOT EXISTS udfs;
-CREATE TABLE if NOT EXISTS udfs.bronze_public.user_abis(
+    CREATE schema if NOT EXISTS udfs_dev.bronze_public;
+CREATE TABLE if NOT EXISTS udfs_dev.bronze_public.user_abis(
         contract_address VARCHAR,
         blockchain VARCHAR,
         abi VARCHAR,
@@ -21,55 +21,53 @@ CASE
         OR NOT REGEXP_LIKE(:CONTRACT_ADDRESS, '0x[a-fA-f0-9]*')) THEN RETURN 'ERROR: Check your contract address format';
         WHEN (LOWER(:BLOCKCHAIN) NOT IN ('ethereum')) THEN RETURN 'ERROR: Unsupported blockchain';
         WHEN (CHECK_JSON(:ABI) IS NOT NULL) THEN RETURN 'ERROR: ABI is not valid JSON format';
-        WHEN (
-            :CONTRACT_ADDRESS IS NULL
-            OR :BLOCKCHAIN IS NULL
-            OR :ABI IS NULL
-            OR :discord_username IS NULL
-        ) THEN RETURN 'ERROR: Please provide all four inputs';
+        WHEN (LENGTH(:ABI :: STRING) <= 1
+        OR LENGTH(:discord_username :: STRING) <= 1) THEN RETURN 'ERROR: Please provide all four inputs';
         WHEN (
             (
                 SELECT
                     1
                 FROM
-                    ethereum.silver.abis
+                    udfs_dev.silver.abis
                 WHERE
-                    abi_hash = CONCAT(LOWER(:contract_address), '-', SHA2(PARSE_JSON(:ABI)))) = 1
-            ) THEN
-            INSERT INTO
-                udfs.bronze_public.user_abis (
-                    contract_address,
-                    blockchain,
-                    abi,
-                    discord_username,
-                    _INSERTED_TIMESTAMP,
-                    duplicate_abi
-                )
-            SELECT
-                LOWER(:CONTRACT_ADDRESS),
-                LOWER(:BLOCKCHAIN),
-                PARSE_JSON(:ABI),
-                :discord_username,
-                SYSDATE() :: TIMESTAMP,
-                TRUE AS duplicate_abi;
-                ELSE
-            INSERT INTO
-                udfs.bronze_public.user_abis (
-                    contract_address,
-                    blockchain,
-                    abi,
-                    discord_username,
-                    _INSERTED_TIMESTAMP,
-                    duplicate_abi
-                )
-            SELECT
-                LOWER(:CONTRACT_ADDRESS),
-                LOWER(:BLOCKCHAIN),
-                PARSE_JSON(:ABI),
-                :discord_username,
-                SYSDATE() :: TIMESTAMP,
-                FALSE AS duplicate_abi;
-        END;
+                    contract_address = LOWER(:contract_address)
+                    AND blockchain = LOWER(:BLOCKCHAIN)
+            ) = 1
+        ) THEN
+        INSERT INTO
+            udfs_dev.bronze_public.user_abis (
+                contract_address,
+                blockchain,
+                abi,
+                discord_username,
+                _INSERTED_TIMESTAMP,
+                duplicate_abi
+            )
+        SELECT
+            LOWER(:CONTRACT_ADDRESS),
+            LOWER(:BLOCKCHAIN),
+            PARSE_JSON(:ABI),
+            :discord_username,
+            SYSDATE() :: TIMESTAMP,
+            TRUE AS duplicate_abi;
+            ELSE
+        INSERT INTO
+            udfs_dev.bronze_public.user_abis (
+                contract_address,
+                blockchain,
+                abi,
+                discord_username,
+                _INSERTED_TIMESTAMP,
+                duplicate_abi
+            )
+        SELECT
+            LOWER(:CONTRACT_ADDRESS),
+            LOWER(:BLOCKCHAIN),
+            PARSE_JSON(:ABI),
+            :discord_username,
+            SYSDATE() :: TIMESTAMP,
+            FALSE AS duplicate_abi;
+    END;
 RETURN 'SUCCESS: ABI added to the decoding queue. Logs should be decoded within 24 hours';
-    END;$$;
+END;$$;
 {%- endmacro %}
