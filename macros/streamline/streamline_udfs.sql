d457e28695bcb5c01d3e53913162f39da9715438
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-01-27 10:24:07 -0500

    Create or replace

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -12,7 +12,11 @@
 {% endmacro %}
 
 {% macro create_udf_call_node() %}
-    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_json_rpc_call(node_url VARCHAR, headers OBJECT, data ARRAY) returns variant api_integration = aws_udf_api AS {% if target.name == "prod" %}
+    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_json_rpc_call(
+        node_url VARCHAR,
+        headers OBJECT,
+        DATA ARRAY
+    ) returns variant api_integration = aws_udf_api AS {% if target.name == "prod" %}
         'https://nvbe90pdg3.execute-api.us-east-1.amazonaws.com/prod/call_node'
     {% else %}
         'https://smro9shis5.execute-api.us-east-1.amazonaws.com/dev/call_node'
@@ -33,8 +37,9 @@
 {% endmacro %}
 
 {% macro create_udf_hex_encode_function() %}
-    CREATE EXTERNAL FUNCTION IF NOT EXISTS streamline.udf_hex_encode_function(
-        string VARCHAR
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_hex_encode_function(
+        STRING VARCHAR
     ) returns text api_integration = aws_udf_api AS {% if target.name == "prod" %}
         'https://nvbe90pdg3.execute-api.us-east-1.amazonaws.com/prod/udf_hex_encode_function'
     {% else %}
