afa1535b279035b70f5efbd38560cd4c6ac51c88
Ryan-Loofy <63126328+Ryan-Loofy@users.noreply.github.com>
2023-02-09 11:12:48 -0500

    Formatting updates

--- macros/streamline/streamline_udfs.sql
+++ macros/streamline/streamline_udfs.sql
@@ -1,5 +1,6 @@
 {% macro create_udf_call_read_batching() %}
-    CREATE OR REPLACE EXTERNAL FUNCTION streamline.udf_json_rpc_read_calls(
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_json_rpc_read_calls(
         secret_name VARCHAR,
         node_url VARCHAR,
         headers OBJECT,
@@ -12,7 +13,8 @@
 {% endmacro %}
 
 {% macro create_udf_api() %}
-    CREATE OR REPLACE EXTERNAL FUNCTION streamline.udf_api(
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_api(
         method VARCHAR,
         url VARCHAR,
         headers OBJECT,
@@ -24,19 +26,27 @@
     {%- endif %};
 {% endmacro %}
 
+{% macro create_udf_call_node() %}
+    CREATE
+    OR REPLACE EXTERNAL FUNCTION streamline.udf_call_node(
+        secret_name VARCHAR,
+        node_url VARCHAR,
+        headers OBJECT,
+        json_data ARRAY
+    ) returns variant api_integration = aws_udf_api AS {% if target.name == "prod" %}
+        'https://nvbe90pdg3.execute-api.us-east-1.amazonaws.com/prod/udf_call_node'
+    {% else %}
+        'https://smro9shis5.execute-api.us-east-1.amazonaws.com/dev/udf_call_node'
+    {%- endif %};
+{% endmacro %}
+
 {% macro create_udf_hex_encode_function() %}
     CREATE
     OR REPLACE EXTERNAL FUNCTION streamline.udf_hex_encode_function(
         STRING VARCHAR
-    ) returns text api_integration = aws_udf_api AS {% if target.name == "prod" %}
+    ) returns variant api_integration = aws_udf_api AS {% if target.name == "prod" %}
         'https://nvbe90pdg3.execute-api.us-east-1.amazonaws.com/prod/udf_hex_encode_function'
     {% else %}
         'https://smro9shis5.execute-api.us-east-1.amazonaws.com/dev/udf_hex_encode_function'
-
-{% macro create_udf_call_node() %}
-    CREATE OR REPLACE EXTERNAL FUNCTION streamline.udf_call_node(secret_name VARCHAR, node_url VARCHAR, headers OBJECT, json_data ARRAY) returns variant api_integration = aws_udf_api AS {% if target.name == "prod" %}
-        'https://nvbe90pdg3.execute-api.us-east-1.amazonaws.com/prod/udf_call_node'
-    {% else %}
-        'https://smro9shis5.execute-api.us-east-1.amazonaws.com/dev/udf_call_node'
     {%- endif %};
 {% endmacro %}
